fab2288f9700f92245ae00d36c133780fa9cb943
Nathan Sutton <nate@sevenwire.com>
2010-06-14 14:39:17 -0500

    Missed one test

--- test/zencoder/account_test.rb
+++ test/zencoder/account_test.rb
@@ -11,7 +11,7 @@ class Zencoder::AccountTest < Test::Unit::TestCase
       setup do
         @url = "#{Zencoder.base_url}/account"
         @params = {:api_key => @api_key}
-        @params_as_json = @params.to_json
+        @params_as_json = MultiJson.encode(@params)
       end
 
       should "POST to the correct url and return a response" do
