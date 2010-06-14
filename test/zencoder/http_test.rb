6056d4034c50b98c2cd32ccebf079a6ca9d2f3b4
Nathan Sutton <nate@sevenwire.com>
2010-06-14 16:18:47 -0500

    Better testing of http

--- test/zencoder/http_test.rb
+++ test/zencoder/http_test.rb
@@ -31,6 +31,44 @@ class Zencoder::HTTPTest < Test::Unit::TestCase
       end
     end
 
+    should "return a Zencoder::Response" do
+      Zencoder::HTTP.http_backend.stubs(:post).returns(stub(:code => 200, :body => '{"some": "hash"}'))
+      Zencoder::HTTP.http_backend.stubs(:put).returns(stub(:code => 200, :body => '{"some": "hash"}'))
+      Zencoder::HTTP.http_backend.stubs(:get).returns(stub(:code => 200, :body => '{"some": "hash"}'))
+      Zencoder::HTTP.http_backend.stubs(:delete).returns(stub(:code => 200, :body => '{"some": "hash"}'))
+
+      assert Zencoder::HTTP.post('https://example.com', '{"some": "hash"}').is_a?(Zencoder::Response)
+      assert Zencoder::HTTP.put('https://example.com', '{"some": "hash"}').is_a?(Zencoder::Response)
+      assert Zencoder::HTTP.get('https://example.com').is_a?(Zencoder::Response)
+      assert Zencoder::HTTP.delete('https://example.com').is_a?(Zencoder::Response)
+    end
+
+    should "store the raw response" do
+      post_stub = stub(:code => 200, :body => '{"some": "hash"}')
+      Zencoder::HTTP.http_backend.stubs(:post).returns(post_stub)
+      assert_equal post_stub, Zencoder::HTTP.post('https://example.com', '{"some": "hash"}').raw_response
+    end
+
+    should "store the raw response body" do
+      Zencoder::HTTP.http_backend.stubs(:post).returns(stub(:code => 200, :body => '{"some": "hash"}'))
+      assert_equal '{"some": "hash"}', Zencoder::HTTP.post('https://example.com', '{"some": "hash"}').raw_body
+    end
+
+    should "store the response code" do
+      Zencoder::HTTP.http_backend.stubs(:post).returns(stub(:code => 200, :body => '{"some": "hash"}'))
+      assert_equal 200, Zencoder::HTTP.post('https://example.com', '{"some": "hash"}').code
+    end
+
+    should "JSON parse the response body" do
+      Zencoder::HTTP.http_backend.stubs(:put).returns(stub(:code => 200, :body => '{"some": "hash"}'))
+      assert_equal({'some' => 'hash'}, Zencoder::HTTP.put('https://example.com', '{"some": "hash"}').body)
+    end
+
+    should "store the raw body if the body fails to be JSON parsed" do
+      Zencoder::HTTP.http_backend.stubs(:put).returns(stub(:code => 200, :body => '{"some": bad json'))
+      assert_equal '{"some": bad json', Zencoder::HTTP.put('https://example.com', '{"some": "hash"}').body
+    end
+
     context ".post" do
       should "call post on the http_backend" do
         Zencoder::HTTP.http_backend.expects(:post).
