890c6f5dfdbcad9b68d1ca3a27aa5bb152b83768
Nathan Sutton <nate@sevenwire.com>
2010-06-14 22:01:32 -0500

    Make project work with jruby

--- Gemfile
+++ Gemfile
@@ -5,5 +5,8 @@ group :test do
   gem "shoulda"
   gem "mocha"
   gem "webmock"
+end
+
+group :native_extensions do
   gem "typhoeus"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,17 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'zencoder'
-require 'typhoeus'
+
+begin
+  require 'typhoeus'
+rescue LoadError # doesn't work for all ruby versions
+  puts
+  puts 'Typhoeus not loaded. If your ruby version supports native extensions'
+  puts 'then consider installing it.'
+  puts
+  puts '   gem install typhoeus'
+  puts
+end
 
 class Test::Unit::TestCase
   include WebMock
