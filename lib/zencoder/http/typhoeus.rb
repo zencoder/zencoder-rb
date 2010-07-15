82d533efabdc9eb8b4c4df84bd152adb14c70ea8
Nathan Sutton <nate@sevenwire.com>
2010-07-14 22:15:19 -0500

    Allow skipping of SSL verification with Typhoeus

--- lib/zencoder/http/typhoeus.rb
+++ lib/zencoder/http/typhoeus.rb
@@ -3,19 +3,27 @@ class Zencoder
     class Typhoeus
 
       def self.post(url, options={})
-        ::Typhoeus::Request.post(url, options)
+        perform(:post, url, options)
       end
 
       def self.put(url, options={})
-        ::Typhoeus::Request.put(url, options)
+        perform(:put, url, options)
       end
 
       def self.get(url, options={})
-        ::Typhoeus::Request.get(url, options)
+        perform(:get, url, options)
       end
 
       def self.delete(url, options={})
-        ::Typhoeus::Request.delete(url, options)
+        perform(:delete, url, options)
+      end
+
+      def self.perform(method, url, options={})
+        if options.delete(:skip_ssl_verify)
+          options[:disable_ssl_peer_verification] = true
+        end
+
+        ::Typhoeus::Request.send(method, url, options)
       end
 
     end
