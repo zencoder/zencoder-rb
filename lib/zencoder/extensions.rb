af296f915a6d7fdcacbf77ed637679cb64ab2319
Nathan Sutton <nate@sevenwire.com>
2010-08-31 14:54:02 -0500

    Recursively handle arrays when setting to be with indifferent access

--- lib/zencoder/extensions.rb
+++ lib/zencoder/extensions.rb
@@ -1,11 +1,27 @@
 class Hash
   def recursive_with_indifferent_access
     hash = with_indifferent_access
+
     hash.each do |key, value|
-      if value.is_a?(Hash)
+      if value.is_a?(Hash) || value.is_a?(Array)
         hash[key] = value.recursive_with_indifferent_access
       end
     end
-    return hash
+
+    hash
+  end
+end
+
+class Array
+  def recursive_with_indifferent_access
+    array = dup
+
+    array.each_with_index do |value, index|
+      if value.is_a?(Hash) || value.is_a?(Array)
+        array[index] = value.recursive_with_indifferent_access
+      end
+    end
+
+    array
   end
 end
