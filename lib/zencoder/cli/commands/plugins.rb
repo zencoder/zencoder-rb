697e34f41430e852b7c47b639e23e3e6648aeea6
Brandon Arbini <brandon@zencoder.com>
2010-11-15 23:18:31 -0600

    Plugin install an uninstall.

--- lib/zencoder/cli/commands/plugins.rb
+++ lib/zencoder/cli/commands/plugins.rb
@@ -1,7 +1,9 @@
 module ZencoderCLI::Command
   class Plugins < Base
 
-    provides "plugins", { "plugins:list" => "Lists installed plugins" }
+    provides "plugins", { "plugins:list" => "Lists installed plugins",
+                          "plugins:install" => "Install a plugin via URL",
+                          "plugins:uninstall" => "Uninstall a plugin" }
 
     class << self
 
@@ -16,25 +18,25 @@ module ZencoderCLI::Command
         end
       end
 
-      # def install(args, global_options, command_options)
-      #   plugin = ZencoderCLI::Plugin.new(args.shift)
-      #   if plugin.install
-      #     begin
-      #       ZencoderCLI::Plugin.load_plugin(plugin.name)
-      #     rescue Exception => e
-      #       installation_failed(plugin, e.message)
-      #     end
-      #     display "#{plugin} installed."
-      #   else
-      #     error "Could not install #{plugin}. Please check the URL and try again."
-      #   end
-      # end
+      def install(args, global_options, command_options)
+        plugin = ZencoderCLI::Plugin.new(args.shift)
+        if plugin.install
+          begin
+            ZencoderCLI::Plugin.load_plugin(plugin.name)
+          rescue Exception => e
+            installation_failed(plugin, e.message)
+          end
+          display "#{plugin} installed."
+        else
+          error "Could not install #{plugin}. Please check the URL and try again."
+        end
+      end
 
-      # def uninstall(args, options)
-      #   plugin = ZencoderCLI::Plugin.new(args.shift)
-      #   plugin.uninstall
-      #   display "#{plugin} uninstalled."
-      # end
+      def uninstall(args, global_options, command_options)
+        plugin = ZencoderCLI::Plugin.new(args.shift)
+        plugin.uninstall
+        display "#{plugin} uninstalled."
+      end
 
     end
   end
