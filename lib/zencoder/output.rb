429f95746666db141cf68df3c9fbddbb24fa70e0
Nathan Sutton <nate@zencoder.com>
2011-12-18 20:40:14 -0600

    Add a method to get output details

--- lib/zencoder/output.rb
+++ lib/zencoder/output.rb
@@ -1,6 +1,10 @@
 module Zencoder
   class Output < Resource
 
+    def self.details(output_id, options={})
+      get("/outputs/#{output_id}", options)
+    end
+
     def self.progress(output_id, options={})
       get("/outputs/#{output_id}/progress", options)
     end
--- test/zencoder/output_test.rb
+++ test/zencoder/output_test.rb
@@ -7,6 +7,18 @@ class Zencoder::OutputTest < Test::Unit::TestCase
       @api_key = 'abc123'
     end
 
+    context ".details" do
+      setup do
+        @output_id = 1
+        @url = "#{Zencoder.base_url}/outputs/#{@output_id}"
+      end
+
+      should "GET the correct url and return a response" do
+        Zencoder::HTTP.stubs(:get).with(@url, :headers => { "Zencoder-Api-Key" => @api_key }).returns(Zencoder::Response.new)
+        assert_equal Zencoder::Response, Zencoder::Output.details(@output_id, :api_key => @api_key).class
+      end
+    end
+
     context ".progress" do
       setup do
         @output_id = 1
