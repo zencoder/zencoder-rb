6927a3dca2a321e81f96da1e516ce707167115a0
Nathan Sutton <nate@zencoder.com>
2011-12-18 20:01:53 -0600

    Removing Zencoder::Resource.apply_api_key as it's no longer used (woo!)

--- lib/zencoder/resource.rb
+++ lib/zencoder/resource.rb
@@ -55,29 +55,6 @@ module Zencoder
       options
     end
 
-    def self.apply_api_key(params, format=nil)
-      if api_key
-        decoded_params = decode(params, format).with_indifferent_access
-
-        if decoded_params[:api_request]
-          decoded_params[:api_request] = decoded_params[:api_request].with_indifferent_access
-        end
-
-        if format.to_s == 'xml'
-          if !(decoded_params[:api_request] && decoded_params[:api_request][:api_key])
-            decoded_params[:api_request] ||= {}.with_indifferent_access
-            decoded_params[:api_request][:api_key] = api_key
-          end
-        else
-          decoded_params['api_key'] = api_key unless decoded_params['api_key']
-        end
-
-        decoded_params
-      else
-        params
-      end
-    end
-
     def self.merge_params(options, params)
       if options[:params]
         options[:params] = options[:params].merge(params)
