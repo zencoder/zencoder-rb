34a2bd00aa9277345c5ab4d05744e960c9f9152d
Adam Kittelson <adam.kittelson@apathydrive.com>
2014-01-03 13:15:15 -0600

    add .all / .vod / .live reporting api endpoints

--- lib/zencoder/report.rb
+++ lib/zencoder/report.rb
@@ -2,6 +2,25 @@ module Zencoder
   class Report < Resource
 
     def self.minutes(options={})
+      get("/reports/minutes", merge_params(*extract_params(options)))
+    end
+
+    def self.all(options={})
+      get("/reports/all", merge_params(*extract_params(options)))
+    end
+
+    def self.live(options={})
+      get("/reports/live", merge_params(*extract_params(options)))
+    end
+
+    def self.vod(options={})
+      get("/reports/vod", merge_params(*extract_params(options)))
+    end
+
+
+  protected
+
+    def self.extract_params(options={})
       options = options.dup
       params = {
         :from => options.delete(:from),
@@ -9,9 +28,7 @@ module Zencoder
         :grouping => options.delete(:grouping)
       }
 
-      params.delete_if { |k, v| v.nil? }
-
-      get("/reports/minutes", merge_params(options, params))
+      return options, params.delete_if { |k, v| v.nil? }
     end
 
   end
